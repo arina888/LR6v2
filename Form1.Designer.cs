884cde581eda1374265d61e06075803cb38c1c84
arinameliakina <melyakina2003@mail.ru>
2023-05-26 02:03:23 +0400

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.discountTextBox = new System.Windows.Forms.TextBox();
+            this.discountCheckBox = new System.Windows.Forms.CheckBox();
+            this.productsListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.AliceBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(226, 359);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(235, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Добавить тариф";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.AliceBlue;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(815, 355);
+            this.button2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(592, 52);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Вывести тариф со средней ценой";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nameTextBox.Location = new System.Drawing.Point(300, 109);
+            this.nameTextBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.nameTextBox.MaxLength = 100;
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(341, 30);
+            this.nameTextBox.TabIndex = 2;
+            this.nameTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.nameTextBox_KeyPress);
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.priceTextBox.Location = new System.Drawing.Point(300, 155);
+            this.priceTextBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.priceTextBox.MaxLength = 20;
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.Size = new System.Drawing.Size(341, 30);
+            this.priceTextBox.TabIndex = 3;
+            // 
+            // discountTextBox
+            // 
+            this.discountTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.discountTextBox.Location = new System.Drawing.Point(317, 299);
+            this.discountTextBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.discountTextBox.MaxLength = 20;
+            this.discountTextBox.Name = "discountTextBox";
+            this.discountTextBox.Size = new System.Drawing.Size(300, 30);
+            this.discountTextBox.TabIndex = 4;
+            // 
+            // discountCheckBox
+            // 
+            this.discountCheckBox.AutoSize = true;
+            this.discountCheckBox.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.discountCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.discountCheckBox.Location = new System.Drawing.Point(55, 301);
+            this.discountCheckBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.discountCheckBox.Name = "discountCheckBox";
+            this.discountCheckBox.Size = new System.Drawing.Size(194, 29);
+            this.discountCheckBox.TabIndex = 5;
+            this.discountCheckBox.Text = "Льготный тариф";
+            this.discountCheckBox.UseVisualStyleBackColor = false;
+            // 
+            // productsListBox
+            // 
+            this.productsListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.productsListBox.FormattingEnabled = true;
+            this.productsListBox.ItemHeight = 25;
+            this.productsListBox.Location = new System.Drawing.Point(814, 109);
+            this.productsListBox.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.productsListBox.Name = "productsListBox";
+            this.productsListBox.ScrollAlwaysVisible = true;
+            this.productsListBox.Size = new System.Drawing.Size(591, 204);
+            this.productsListBox.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(268, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 36);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "АТС";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(153, 64);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(253, 29);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Добавление тарифа:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(10, 109);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(183, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Название тарифа:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(-6, 155);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(232, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Цена тарифа(руб/мин):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(48, 235);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(475, 50);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Если тариф со скидкой, то отметьте это в поле \r\n\"Льготный тариф\", введя скидку (в" +
+    " процентах):";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(996, 64);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(203, 29);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Список тарифов";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(1480, 468);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.discountTextBox);
+            this.Controls.Add(this.discountCheckBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.productsListBox);
+            this.Controls.Add(this.priceTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.button2);
+            this.Font = new System.Drawing.Font("Perpetua Titling MT", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа 6";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.TextBox discountTextBox;
+        private System.Windows.Forms.CheckBox discountCheckBox;
+        private System.Windows.Forms.ListBox productsListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private List<Tariff> tariffs = new List<Tariff>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = nameTextBox.Text;
+            if (string.IsNullOrEmpty(name))
+            {
+                ShowErrorMessage("Введите название товара в поле \"Название тарифа\"");
+                return;
+            }
+
+            if (!decimal.TryParse(priceTextBox.Text, out decimal price) || price <= 0)
+            {
+                ShowErrorMessage("Введите положительное число в поле \"Цена (руб/мин)\"");
+                return;
+            }
+
+            double discount = 0;
+            if (discountCheckBox.Checked)
+            {
+                if (!double.TryParse(discountTextBox.Text, out discount) || discount < 0 || discount > 100)
+                {
+                    ShowErrorMessage("Введите положительное число в поле \"Льготный тариф\"");
+                    return;
+                }
+            }
+
+            Price priceWithDiscount = new Price(price, discount);
+            Price priceWithoutDiscount = new Price(price, 0);
+            Tariff tariff = new Tariff(name, priceWithDiscount, priceWithoutDiscount);
+            tariffs.Add(tariff);
+            productsListBox.Items.Add(tariff.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (tariffs.Count == 0)
+            {
+                ShowErrorMessage("Список пуст");
+                return;
+            }
+            int  a = 0;
+            double sum = 0;
+            double rez = 0;
+            Tariff cheapestProduct = tariffs[0];
+            for (int i = 0; i < tariffs.Count; i++)
+            {
+                sum += (double)(tariffs[i].PriceWithDiscount.AmountWithDiscount);
+                /*if (tariffs[i].PriceWithDiscount.AmountWithDiscount < cheapestProduct.PriceWithDiscount.AmountWithDiscount)
+                {
+                    cheapestProduct = tariffs[i];
+                }*/
+                a++;
+            }
+             rez = Math.Round(((double)sum / a),2);
+           
+
+            MessageBox.Show(rez.ToString());
+            //MessageBox.Show($"Тариф с средней ценой: {cheapestProduct.Name} - цена: {cheapestProduct.PriceWithDiscount.AmountWithDiscount} руб/мин");
+        }
+
+        private void ShowErrorMessage(string message)
+        {
+            MessageBox.Show(message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void nameTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if(number == ' ')
+            {
+                e.Handled = true;
+            }
+        }
+    }
+
+    public class Store
+    {
+        public Tariff Tariff { get; }
+
+        public Tariff Tariff1
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Store(Tariff tariff)
+        {
+            Tariff = tariff;
+        }
+    }
+
+    public class Tariff
+    {
+        public string Name { get; }
+        public Price PriceWithDiscount { get; }
+        public Price PriceWithoutDiscount { get; }
+
+        public Price Price
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Tariff(string name, Price priceWithDiscount, Price priceWithoutDiscount)
+        {
+            Name = name;
+            PriceWithDiscount = priceWithDiscount;
+            PriceWithoutDiscount = priceWithoutDiscount;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - цена по льготе: {PriceWithDiscount.AmountWithDiscount} руб.";
+        }
+    }
+
+    public class Price : IComparable<Price>
+    {
+        public decimal Amount { get; }
+        public double Discount { get; }
+
+        public decimal AmountWithDiscount => Amount * (decimal)(1 - Discount / 100);
+
+        public Price(decimal amount, double discount)
+        {
+            Amount = amount;
+            Discount = discount;
+        }
+
+        public int CompareTo(Price other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+            return AmountWithDiscount.CompareTo(other.AmountWithDiscount);
+        }
+    }
+
+    public class Discount
+    {
+        public double Value { get; }
+
+        public Discount(double value)
+        {
+            Value = value;
+        }
+    }
+
+    public class DiscountedPrice : Price
+    {
+        public Discount Discount { get; }
+
+        public DiscountedPrice(decimal amount, Discount discount)
+            : base(amount, discount.Value)
+        {
+            Discount = discount;
+        }
+    }
+
+    public class RegularPrice : Price
+    {
+        public RegularPrice(decimal amount)
+         : base(amount, 0)
+        {
+        }
+    }
+}
